9de248c93d6047c3bcf84c77fc11720fdcc9697c
Chinmay <chinmaybhargava95022002@gmail.com>
2023-04-05 23:43:35 +0530

    Uploading 1st time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [inputdata, setInputdata] = useState({
+    height:"0",
+    length:"0",
+    water:"0"
+  })
+  const [state, setState] = useState ({
+
+  options: {
+    chart: {
+      height: 500,
+      type: "line",
+          stacked: false
+        },
+        dataLabels: {
+          enabled: false
+        }
+      },
+        colors: ["#FF1654", "#247BA0"],
+        series: [{
+          data: [
+            {
+              x: 0,
+              y: 15
+          },{
+              x: 12,
+              y: 15
+          }, {
+              x: 18,
+              y: 12
+          },{
+            x: 30,
+            y: 12
+        }
+        ],
+      }],
+      xaxis: {
+        type: 'numeric'
+      },
+        stroke: {
+          width: [5, 5]
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: "80%"
+          }
+        },     
+        tooltip: {
+          shared: false,
+          intersect: true,
+          x: {
+            show: false
+          }
+        },
+        legend: {
+          horizontalAlign: "left",
+          offsetX: 40
+        }
+      }
+)
+
+function changehandle(e) {
+  setInputdata({...inputdata,[e.target.height]:e.target.value})
 }
 
-export default App;
+return (
+  <div className="App">
+    <h1>React Apex Chart</h1>
+
+    <br />
+    <br />
+
+    <ReactApexChart 
+    options={state.options}
+    series={state.series}
+    type="line"
+    height={350}
+    width={800}
+    />
+    <div className="form">
+      <h1>Slope Properties</h1>
+    Height : <input type="number" name="height" id="form"  onChange={changehandle} /><br />
+    Length : <input type="number" name="length" id="form" value={inputdata.length} onChange={changehandle}/><br />
+    Water-level : <input type="number" name="water" id="form" value={inputdata.water} onChange={changehandle} /><br/>
+    <button>Analyse</button>
+    </div>
+  </div>
+);
+}
+
+export default App
