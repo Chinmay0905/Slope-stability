2d17e61dc174bcd1d7ffeaf1f17bf9359046a252
Chinmay <chinmaybhargava95022002@gmail.com>
2023-11-21 18:54:59 +0530

    Calculation changes

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,105 +1,79 @@
 import React, { useState } from "react";
-import ReactApexChart from "react-apexcharts";
+
+function Graph() {
+
+  const [height, setHeight] = useState();
+  const [width, setWidth] = useState();
+  const [angle, setAngle] = useState();
+  const [wHeight, setWHeight] = useState();
+  const [print, setPrint] = useState(false);
 
 
-function Graph(){
-        const styles = {
-                display: {
-                        display : "flex",
-                        justifyContent : "right",
-                        padding : "35px",
-                        margin : "71px",
-                }
-        }
-        const [inputdata, setInputdata] = useState({
-                height:"0",
-                length:"0",
-                water:"0"
-              })
-              const [state, setState] = useState ({
-            
-              options: {
-                chart: {
-                  height: 500,
-                  type: "line",
-                      stacked: false
-                    },
-                    dataLabels: {
-                      enabled: false
-                    }
-                  },
-                    colors: ["#247BA0"],
-                    series: [{
-                      data: [
-                        {
-                          x: 0,
-                          y: 15
-                      },{
-                          x: 12,
-                          y: 15
-                      }, {
-                          x: 18,
-                          y: 12
-                      },{
-                        x: 30,
-                        y: 12
-                    }
-                    ],
-                  }],
-                  xaxis: {
-                    type: 'numeric'
-                  },
-                    stroke: {
-                      width: [5, 5]
-                    },
-                    plotOptions: {
-                      bar: {
-                        columnWidth: "80%"
-                      }
-                    },     
-                    tooltip: {
-                      shared: false,
-                      intersect: true,
-                      x: {
-                        show: false
-                      }
-                    },
-                    legend: {
-                      horizontalAlign: "left",
-                      offsetX: 40
-                    }
-                  }
-            )
-            function changehandle(e) {
-                setInputdata({...inputdata,[e.target.height]:e.target.value})
-              }
+  function getHeight(e) {
+    setHeight(e.target.value);
+    setPrint(false);
+  }
+  function getWidth(e) {
+    setWidth(e.target.value);
+    setPrint(false);
+  }
+  
+  function getAngle(e) {
+    setAngle(e.target.value);
+    setPrint(false);
+  }
+  function getWaterHeight(e) {
+    setWHeight(e.target.value);
+    setPrint(false);
+  }
+
   return (
-        <>
-        <div className="whole"style={styles.display}>
-        <ReactApexChart 
-        options={state.options}
-        series={state.series}
-        type="line"
-        height={350}
-        width={800}
-        />
-        <div className="container" style={{fontSize:"12px"}}>
-        <div className="mx-auto col-10 col-md-8 col-lg-6">
-        <h1>Slope Properties</h1>
+    <>
+    <br />
+    <br />
+      <h1 className="text-center">
+        Slope Stability Calculation
+      </h1>
+      <br />
+      <br />
+
+      <div className="w-50 p-3 container d-flex justify-content-center ">
+        <div className="form">
+        <form>
+            <div className="mb-3">
+              <label className="h4" >Height of slice</label>
+              <input className="form-control form-control" type="number" value={height} onChange={getHeight} id="slope_length" placeholder="Enter Slope Height" aria-label=".form-control-lg example"/>
+            </div>
+            <div className="mb-3">
+              <label className="h4" >Width of slice</label>
+              <input className="form-control form-control" type="number" value={width} onChange={getWidth} id="slope_height" placeholder="Enter Unit Weight" aria-label=".form-control-lg example"/>
+            </div>
+            <div className="mb-3">
+              <label className="h4" >Slope Angle of the base of slice</label>
+              <input className="form-control form-control" type="number" value={angle} onChange={getAngle} id="slope_height" placeholder="Enter Slope Angle (In radians)" aria-label=".form-control-lg example"/>
+            </div>
+            <div className="mb-3">
+              <label className="h4" >Height of water below crest level</label>
+              <input className="form-control form-control" type="number" value={wHeight} onChange={getWaterHeight} id="slope_height" placeholder="Enter Water Height" aria-label=".form-control-lg example"/>
+            </div>
+        </form>
+          <button className="btn btn-primary h3" id="btn1" onClick={() => {setPrint(true)}}>Analyse</button>
+        </div>
         <br />
-        <div class="input-group mb-3 w-50 ">
-        <span class="input-group-text" id="basic-addon1">Height</span>
-        <input type="number" class="form-control" aria-label="Height" aria-describedby="basic-addon1"/>
-        </div>
-        <div class="input-group mb-3 w-50">
-        <span class="input-group-text" id="basic-addon1">Length</span>
-        <input type="number" class="form-control" aria-label="length" aria-describedby="basic-addon1" />
-        </div>
-        <button type="button" class="btn btn-primary">Analyse</button>
-        </div>
-        </div>
-        </div>
-      </>
+        <br />
+
+       </div>
+      {
+        print?
+        <h2 className="text-center">The slope is {((5*width + (20*height*width)*0.57735)/20*height*width*0.027) >=1 ? "stable" : "unstable"}
+        <h4> Safety Factor = {(5*width + (20*height*width)*0.57735)/20*height*width*0.027} </h4>
+         
+          </h2>
+        
+        : null
+        
+      }
+    </>
   )
 }
 
